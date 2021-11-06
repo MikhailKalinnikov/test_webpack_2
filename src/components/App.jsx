14cfa1b461204219952929f0c58fb27842473a6d
Mikhail.CallInNickOff <mihalka999@gmail.com>
2021-11-06 16:39:52 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "../styles/index.scss";
+
+const App = () => {
+  return (
+    <main>
+      <section>
+        <h1>Hello, React with Webpack!</h1>
+      </section>
+    </main>
+  );
+};
+
+export default App;
